04d11cc58f49216fac1d7f840e62868f7ad7e672
Timothy Wesley <timothy.wesley99@yahoo.com>
2025-04-27 02:41:05 -0400

    organized it a bit better and added spideypedia

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let batman_button = document.querySelector('#batman-game-button')
-let camera_button = document.querySelector('#security-cam-button')
-batman_button.addEventListener("click", () => {
+let batmanButton = document.querySelector('#batman-game-button')
+let cameraButton = document.querySelector('#security-cam-button')
+batmanButton.addEventListener("click", () => {
     window.location.href = "https://github.com/Timwes21/BatmanGame"
 })
 
-camera_button.addEventListener("click", () => {
+cameraButton.addEventListener("click", () => {
     window.location.href = "https://github.com/Timwes21/pizza_time"
 })
