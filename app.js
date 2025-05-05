edf0dc6f2e6235ddaf56b7162b8800e670bdd3cf
Timothy Wesley <timothy.wesley99@yahoo.com>
2025-05-05 00:53:36 -0400

    added last commit dates

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-let batmanButton = document.querySelector('#batman-game-button')
-let cameraButton = document.querySelector('#security-cam-button')
-batmanButton.addEventListener("click", () => {
-    window.location.href = "https://github.com/Timwes21/BatmanGame"
-})
+const qs= (e) => document.querySelector(e);
+const BATMANGAMEREPO = 'BatmanGame';
+const PIZZATIMEREPO = 'pizza_time';
+const SPIDEYPEDIAREPO = 'spideypedia';
 
-cameraButton.addEventListener("click", () => {
-    window.location.href = "https://github.com/Timwes21/pizza_time"
-})
+let batmanButton = qs('#batman-game-button')
+let pizzaTimeButton = qs('#pizza-time-button')
+let spideypediaButton = qs("#spideypedia-button")
+
+
+function goToRepo(button, repo){
+    button.addEventListener("click", () => {
+        window.location.href = "https://github.com/Timwes21/" + repo;
+    })
+}
+goToRepo(batmanButton, BATMANGAMEREPO);
+goToRepo(pizzaTimeButton, PIZZATIMEREPO);
+goToRepo(spideypediaButton, SPIDEYPEDIAREPO);
+
+
+(async function(){
+    qs("#last-commit-spideypedia").innerText = await getLastCommitDate("spideypedia");
+    qs("#last-commit-pizza-time").innerText = await getLastCommitDate("pizza_time");
+    qs("#last-commit-batman-game").innerText = await getLastCommitDate("BatmanGame");
+})();
+
+async function getLastCommitDate(project){
+    const result = await fetch(`https://api.github.com/repos/timwes21/${project}/commits?per_page=1`);
+    const date = result.headers.get("last-modified");
+    const dateArray = date.split(" ");
+    return dateArray[2] + " " + dateArray[1] + ", " + dateArray[3];
+}
+
+
