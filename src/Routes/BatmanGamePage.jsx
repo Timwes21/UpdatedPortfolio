4dec5c27d7adf66af3a2f00623f4a2a242532111
Timothy Wesley <timothywesley@Timothys-MacBook-Air.local>
2025-07-02 16:44:31 -0400

    updated the batman exe file

--- src/Routes/BatmanGamePage.jsx
+++ src/Routes/BatmanGamePage.jsx
@@ -2,6 +2,9 @@ import NavBar from "../components/NavBar";
 import gameScreenshot from "../assets/batman-page/gameplay1.png";
 
 export default function BatmanGamePage(){
+    const download = () => {
+        window.open("BatmanGame-1.0.exe")
+    }
     return (
         <div className="batman-page">
             <NavBar/>
@@ -11,7 +14,7 @@ export default function BatmanGamePage(){
                         <p className="batman-game-description">The player must beat all enemies in each wave. As you progress, your health increases. but so does the diffulcty of the waves. With a small arsenal of smoke grenades and batarangs, along with your martial arts prowess, the enemies have as much fighting chance as you!</p>
                     </div>
                     <div className="download-button-con">
-                        <button>Download Game</button>
+                        <button onClick={download} className="download-game-button">Download for Windows</button>
                     </div>
                 </div>
                 <br />
